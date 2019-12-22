fce60931a432beb777d9b010aa2a89b0f5ae68a6
notchla <liso.lorenzo@gmail.com>
2019-12-22 16:34:40 +0100

    added id in parents condition

--- DriveLogger.py
+++ DriveLogger.py
@@ -15,8 +15,7 @@ from string import ascii_uppercase
 import traceback
 
 ''' TODO:
-    writecode to upload the file in a folder,
-    fix revision index if the revision and the current file are uploaded in a cron_time window
+    writecode to upload the file in a folder
     '''
 
 # If modifying these scopes, delete the file token.pickle.
@@ -233,7 +232,7 @@ def main():
     while True:
         # Call the Drive v3 API
         response = service.files().list(
-            q = "mimeType='application/vnd.oasis.opendocument.spreadsheet' and trashed=false", pageSize=100, fields="nextPageToken, files(id,name,modifiedTime,lastModifyingUser)", pageToken=page_token).execute()
+            q = "mimeType='application/vnd.oasis.opendocument.spreadsheet' and trashed=false and ('0Bx4ptgObUFZfaUw4ekRpdXhyNzQ' in parents or '0Bx4ptgObUFZfdlhWMnpZWWtUbzA' in parents)", pageSize=100, fields="nextPageToken, files(id,name,modifiedTime,lastModifyingUser)", pageToken=page_token).execute()
         items = response.get('files', [])
 
         current_datetime = datetime.datetime.utcnow()
